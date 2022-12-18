ee368f5b738fba49fcd82b00627d9274d06b484b
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2022-12-18 12:55:43 -0300

    feat: create function to filter names

--- src/services/filterNames.ts
+++ src/services/filterNames.ts
@@ -1,3 +1,24 @@
-export const filterNames = (names) => {
-  console.log(names);
+const newNames = []
+let count = 0
+
+export const filterNames = (names: string[]) => {
+  names.forEach(name => {
+
+    // console.log(name.slice(0, 2), name)
+
+    if (name.slice(0, 2) === 'MA' && name.match(/DI/)) {
+      for (let i = 0; i < name.length; i++) {
+        if (name[i].match(/[aeiou]/ig)) {
+          count += 1;
+        }
+      }
+
+      if (count === 4) {
+        newNames.push(name)
+      } 
+    }
+
+    count = 0
+  })
+  console.log(newNames)
 }
