cd2c0030ca1169efad7cd4f83328828f02ad699d
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2022-12-17 22:55:17 -0300

    feat: create function to get the names from names.csv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { filterNames } from "./services/filterNames";
+
+const csv = require('csv-parser')
 const fs = require('fs')
+const names = [];
+
+fs.createReadStream('src/data/names.csv')
+  .pipe(csv())
+  .on('data', (data) => names.push(data.first_name))
+  .on('end', () => {
+    filterNames(names)
+  });
---
+++ src/services/filterNames.ts
@@ -0,0 +1,3 @@
+export const filterNames = (names) => {
+  console.log(names);
+}
